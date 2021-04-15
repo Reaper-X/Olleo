2d762c45a551f673ed3efd362b57dbc04c22f146
Reaper-X <paulmichaeloliva@gmail.com>
2021-04-15 01:45:01 -0400

    Forgive minor changes but now it displays

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React, { Component } from "react"
+import { render } from "react-dom"
 
-export default function Home() {
-  return <div>Olleo is in the building!!! Damnnit!!!</div>
+class index extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Olleo is in the building!!! Damnnit!!!</p>
+            </div>
+        );
+    }
 }
+
+export default index;
